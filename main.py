2df368ea0662d852bb5d87c0771d350c9faf6cfc
Thomas-Holtvedt <thomas.holtvedt@gmail.com>
2025-08-19 15:59:37 +0200

    feat: Main script
    Added main script with plans on how to structure project.

---
+++ main.py
@@ -0,0 +1,10 @@
+# import picture
+
+# Voronoi drawing
+
+
+# Territories, all provinces in grid?
+
+# Output both history
+
+# Out put picture + voronoi start points + territory files +provinces.txt + province history files
